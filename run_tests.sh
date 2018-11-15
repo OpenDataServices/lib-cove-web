6f16860327094d515a07f455351ac670889c7622
James Baster <james.baster@opendataservices.coop>
2018-11-15 17:19:27 +0000

    Initial commit

---
+++ run_tests.sh
@@ -0,0 +1,1 @@
+DJANGO_SETTINGS_MODULE=cove.settings py.test cove --cov --cov-report= $@
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+install_requires = []
+
+with open('./requirements.in') as requirements_txt:
+    requirements = requirements_txt.read().strip().splitlines()
+    for requirement in requirements:
+        if requirement.startswith('#'):
+            continue
+        elif requirement.startswith('-e '):
+            install_requires.append(requirement.split('=')[1])
+        else:
+            install_requires.append(requirement)
+
+setup(
+    name='libcoveweb',
+    version='0.0.0',
+    author='Open Data Services',
+    author_email='code@opendataservices.coop',
+    packages=['cove', 'cove.input', 'cove.dataload', 'cove.lib'],
+    scripts=['manage.py'],
+    url='https://github.com/OpenDataServices/xxxxxxxxxxxxxxxxxxxx',
+    description='',
+    classifiers=[
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+    ],
+    install_requires=install_requires,
+)
