255084829b58de3f99b4776724b4981312a37ede
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-04-24 12:06:44 -0400

    Configure Sentry to not send django.security.DisallowedHost errors

--- cove/settings.py
+++ cove/settings.py
@@ -72,7 +72,9 @@ ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 if env('SENTRY_DSN'):
     import sentry_sdk
     from sentry_sdk.integrations.django import DjangoIntegration
+    from sentry_sdk.integrations.logging import ignore_logger
 
+    ignore_logger('django.security.DisallowedHost')
     sentry_sdk.init(
         dsn=env('SENTRY_DSN'),
         integrations=[DjangoIntegration()]
