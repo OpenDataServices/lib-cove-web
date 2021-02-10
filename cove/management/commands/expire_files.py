3d3c344469bf2be79c3b8d3786b51669aa6ed784
Ben Webb <ben.webb@opendataservices.coop>
2021-02-10 09:42:14 +0000

    management cmds: Number of days before files expire is now configurable
    https://github.com/open-contracting/cove-ocds/issues/142

--- cove/management/commands/expire_files.py
+++ cove/management/commands/expire_files.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from cove.input.models import SuppliedData
+from django.conf import settings
 from django.utils import timezone
 from datetime import timedelta
 import shutil
@@ -9,7 +10,7 @@ class Command(BaseCommand):
     help = 'Delete files that are older than 7 days'
 
     def handle(self, *args, **options):
-        old_data = SuppliedData.objects.filter(created__lt=timezone.now() - timedelta(days=7))
+        old_data = SuppliedData.objects.filter(created__lt=timezone.now() - timedelta(days=getattr(settings, 'DELETE_FILES_AFTER_DAYS', 7)))
         for supplied_data in old_data:
             try:
                 shutil.rmtree(supplied_data.upload_dir())
