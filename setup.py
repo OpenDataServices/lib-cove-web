fd9c70976055d3263049c2b5223022bb74222ac8
Ben Webb <ben.webb@opendataservices.coop>
2023-02-17 16:15:40 +0000

    release: v0.26.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ install_requires = []
 
 setup(
     name='libcoveweb',
-    version='0.25.0',
+    version='0.26.0',
     author='Open Data Services',
     author_email='code@opendataservices.coop',
     packages=find_packages(),
