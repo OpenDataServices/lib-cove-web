2df304f86328d340b079801613a30f237155cf1a
Bee Webb <bee.webb@opendataservices.coop>
2024-05-29 16:44:31 +0100

    release: v0.30.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ install_requires = []
 
 setup(
     name='libcoveweb',
-    version='0.30.2',
+    version='0.30.3',
     author='Open Data Services',
     author_email='code@opendataservices.coop',
     packages=find_packages(),
